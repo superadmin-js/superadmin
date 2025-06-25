838b70842e8bd5f328e8c3429b20217100f41951
Michał Kędrzyński <kedrzu@gmail.com>
2025-06-25 14:00:18 +0200

    chore: package cleanup

--- packages/client/src/services/App.ts
+++ packages/client/src/services/App.ts
@@ -1,6 +1,11 @@
+import { defineInterface } from '@nzyme/ioc';
 import type { App as VueApp } from 'vue';
 
-import { defineInterface } from '@nzyme/ioc';
-
+/**
+ *
+ */
 export type App = VueApp<HTMLElement>;
+/**
+ *
+ */
 export const App = defineInterface<App>({ name: 'App' });
