0e620ae530ac77c01af95d91205f6dedeaf71ddd
Michał Kędrzyński <kedrzu@sigma.clinic>
2026-01-26 11:27:37 +0100

    [SIG-279] Submodule changes

--- packages/utils/vitest.config.ts
+++ packages/utils/vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         include: ['**/*.test.ts'],
+        typecheck: {
+            enabled: true,
+            checker: 'tsc',
+            include: ['src/**/*.test.ts'],
+        },
     },
 });
