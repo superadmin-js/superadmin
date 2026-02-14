d78e10103355658db5222f553947c13ccf240464
Michał Kędrzyński <kedrzu@sigma.clinic>
2026-02-14 12:38:04 +0100

    [SIG-307] Submodule changes

--- packages/superadmin/src/cli.ts
+++ packages/superadmin/src/cli.ts
@@ -3,12 +3,9 @@
 import { execute } from '@nzyme/cli/execute.js';
 import { initialize } from '@nzyme/cli/initialize.js';
 import { loadEnvVariables } from '@nzyme/project-utils/loadEnvVariables.js';
-import { register as registerTsNode } from 'ts-node';
-
 import { BuildCommand } from './commands/BuildCommand.js';
 import { DevCommand } from './commands/DevCommand.js';
 
-registerTsNode({ esm: true });
 loadEnvVariables();
 initialize();
 
