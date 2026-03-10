84d590c75d3b0ed61d678828ea19067faeddb0a6
Michał Kędrzyński <kedrzu@sigma.clinic>
2026-03-10 15:44:53 +0100

    Remove empty JSDoc comments from ExecuteActionEndpoint

--- packages/server/src/endpoints/ExecuteActionEndpoint.ts
+++ packages/server/src/endpoints/ExecuteActionEndpoint.ts
@@ -1,10 +1,7 @@
 import { defineEndpoint } from '@nzyme/rpc/defineEndpoint.js';
 import * as z from '@zod/mini';
 
-export /**
- *
- */
-const ExecuteActionEndpoint = defineEndpoint({
+export const ExecuteActionEndpoint = defineEndpoint({
     name: 'ExecuteAction',
     input: z.object({
         action: z.string(),
