574e00021323124284d189fb10302774baa89e86
Michał Kędrzyński <kedrzu@sigma.clinic>
2026-03-12 17:20:18 +0100

    fix: fixed cli bin [SIG-335] (#57)

---
+++ packages/superadmin/bin/cli.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env bun
+
+await import('../dist/cli.js');
--- packages/superadmin/eslint.config.js
+++ packages/superadmin/eslint.config.js
@@ -1,7 +1,9 @@
 import { common, packageJson, typescript, jsdoc } from '@nzyme/eslint';
+import { globalIgnores } from 'eslint/config';
 
 export default [
     //
+    globalIgnores(['./bin/**/*']),
     ...common(),
     ...typescript({
         target: 'node',
